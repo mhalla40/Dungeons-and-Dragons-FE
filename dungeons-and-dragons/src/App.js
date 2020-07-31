6f8429fc0d04688ec0cbf657bfb810cd3ebd5d73
Michael Hall <mhalla40@gmail.com>
2020-07-31 11:41:02 -0400

    grabbed api from url

---
+++ dungeons-and-dragons/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+let url = "https://dungeons-and-dragons40.herokuapp.com/monster";
+
+//getting the api from url
+const options = {
+  method: "GET",
+  headers: {
+    Accept: "application/json",
+  },
+};
+
+export default App;
